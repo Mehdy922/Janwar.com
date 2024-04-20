497e105f7c64a545cc0ef3955fa3c0c8234367a6
Muhammad Hasnain Fatmi <hasnainfatmi22@gmail.com>
2024-04-20 10:52:53 +0500

    Implementation

---
+++ V2/Janwar/src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Root from "./routes/root";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    <App />
+  </React.StrictMode>,
+)
--- frontend/src/views/auth/Login.jsx
+++ frontend/src/views/auth/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
           <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30  my-4">
             <h1 className="text-4x1 text-whitefont-bold text-center mb-6">Login</h1>
             <form action="">
-            <div className="relative my-4">
+            <div className="relati*ve my-4">
               <input type="email" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-non focus:text-white focus:border-blue-600 peer" />
               <label htmlFor="" className="absolute text-sm text-white duration-300 transform translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus: text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Your Email</label>
             </div>
